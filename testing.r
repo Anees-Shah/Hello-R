3d1f1e6f6900d014d78307d875e7864b2c35f88f
Anees Ahmed Shah <aneesahmed.shah@gmail.com>
2021-09-04 02:52:13 +0500

    Testing lines added

--- testing.r
+++ testing.r
@@ -1,3 +1,9 @@
 # This is from Git
-Print ("This is from Git")
+print("This is from Git")
+
+Name = "Anees"
+Version = "RStudio - in writing"
+
+print(name)
+print("These lines has been written in R studio")
 
